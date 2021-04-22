9e697135d454363702f9f164175ebcd5767f268c
WS325813 <WS325813@Weston.ac.uk>
2021-04-22 19:12:40 +0100

    Create admin on start

--- gamecart/src/database_schema.cpp
+++ gamecart/src/database_schema.cpp
@@ -10,9 +10,24 @@ CREATE TABLE IF NOT EXISTS User
     is_admin      INTEGER NOT NULL
 ))";
 
+
+static constexpr auto MAKE_ADMIN = R"((
+INSERT OR IGNORE INTO User
+    (username, date_of_birth, is_admin)
+VALUES
+    (?, ?, 1)
+))";
+
 void DatabaseSchema::create_tables(Database& database)
 {
     database
         .prepare(USER_TABLE)
         .execute();
+
+    database
+        .prepare(MAKE_ADMIN)
+        .bind_text("admin")
+        .bind_text("01/01/1970")
+        .execute();
 }
+ 
