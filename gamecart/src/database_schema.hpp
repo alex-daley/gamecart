d8228e0f9546491c531750d8963ac014e95b0b66
WS325813 <WS325813@Weston.ac.uk>
2021-04-23 15:10:11 +0100

    Removed existing code

--- gamecart/src/database_schema.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class Database;
-
-namespace DatabaseSchema
-{
-    // Creates Gamecart database tables if they do not already exist.
-    void create_tables(Database& database);
-}
--- gamecart/src/main.cpp
+++ gamecart/src/main.cpp
@@ -1,34 +1,4 @@
-#include <iostream>
-#include <charconv>
-#include <string>
-#include <optional>
-#include <vector>
-#include <memory>
 
-#include "application.hpp"
-#include "console.hpp"
-#include "database.hpp"
-#include "database_schema.hpp"
-
-int main() 
-{
-    Console console;
-    std::unique_ptr<Database> database;
-
-    try
-    {
-        database = std::make_unique<Database>("gamecart.db");
-        DatabaseSchema::create_tables(*database);
-    }
-    catch (const std::runtime_error& err)
-    {
-        console << "Failed to initialise database\n";
-        console << err.what();
-        return 1;
-    }
-
-    Application app(console, database.get());
-    app.run();
-
+int main() {
     return 0;
 } 
